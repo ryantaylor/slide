80b9c8d8fffd3dd99c3ac1196076cc581157e11b
ryantaylor <ryan.taylor9@gmail.com>
2013-03-12 22:12:50 -0700

    bug fixes

--- scripts/zoom.js
+++ scripts/zoom.js
@@ -1,16 +1,18 @@
-var mouseX = 0, mouseY = 0;
-$(document).mousemove(function(e){
-   mouseX = e.pageX;
-   mouseY = e.pageY;
-});
+$(document).ready(function() {
+	var mouseX = 0, mouseY = 0;
+	$(document).mousemove(function(e){
+	   mouseX = e.pageX;
+	   mouseY = e.pageY;
+	});
 
-// cache the selector
-var follower = $("#follower");
-var xp = 0, yp = 0;
-var loop = setInterval(function(){
-    // change 12 to alter damping higher is slower
-    xp += (mouseX - 62 - xp) / 5;
-    yp += (mouseY - 62 - yp) / 5;
-    follower.css({left:xp, top:yp});
-    
-}, 30);
+	// cache the selector
+	var follower = $("#follower");
+	var xp = 0, yp = 0;
+	var loop = setInterval(function(){
+		// change 12 to alter damping higher is slower
+		xp += (mouseX - 62 - xp) / 5;
+		yp += (mouseY - 62 - yp) / 5;
+		follower.css({left:xp, top:yp});
+		
+	}, 30);
+});
