a2d3aece211895913f3d08c59650629ba4787f1d
ryantaylor <ryan.taylor9@gmail.com>
2013-03-19 14:26:54 -0700

    basic scroll animation

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -3,11 +3,34 @@ $(document).ready(function() {
 	var element = $('#green');
 	var backdrop = $('#window-scroll');
 	var height = backdrop.outerHeight();
+	var width = backdrop.width();
+	var borderLeft = width / 2;
+	var borderRight = width * 1.5;
+	
+	
+	var positionX, positionY, relativeY, alpha;
 	var THRESHOLD = 375;
+	var BASE_Y = 505;
+	var MIDDLE = width;
 
 	$(document).scroll(function(e) {
-		if (window.scrollY >= THRESHOLD) {
-			element.css('top', 505 + window.scrollY - THRESHOLD);
+		relativeY = window.scrollY - THRESHOLD;
+		if (relativeY >= 0) {
+			if (relativeY <= 500) {
+				positionX = MIDDLE - relativeY / (500 / borderLeft);
+				positionY = BASE_Y + relativeY;
+				alpha = 0.5 + 0.5 * relativeY / 500;
+				element.animate({top: positionY,
+								 left: positionX,
+								 background: 'rgba(0,255,0,' + alpha + ')'}, 0);
+			}
+			//position = BASEX + window.scrollY - THRESHOLD;
+			//element.animate({top: position}, 0);
+		}
+		else {
+			element.animate({top: BASE_Y,
+							 left: '50%',
+							 background: 'rgba(0,255,0,0.5)'}, 0);
 		}
 	});
 });
